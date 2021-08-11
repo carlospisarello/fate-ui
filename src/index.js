e2a7ac0f77e6c414f70a57479ad9de824914d97b
carlospisarello <carlosjpisarello@gmail.com>
2021-08-11 16:20:54 -0300

    feat(theme): add context api based theme implementation
    BREAKING CHANGE:
    context api based theme implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './styles.module.css'
+export { ThemeProvider } from './theme/themeContext'
 
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
